32a90a4a0cc76a0f1af84f2522f61c68890f2990
Kaue Negrini Ciglioni <kaueciglioni@hotmail.com>
2024-05-17 16:52:56 -0300

    Atualização e utilização do node.js

---
+++ Aula 11/index.js
@@ -0,0 +1,14 @@
+//console.log('O console funcionou perfeitamente')//
+
+var vel = 20
+console.log(`A velocidade do seu carro é ${vel}`)
+
+if (vel>60) {
+    console.log(`Você ultrapassou a velocidade permitida, MULTADO!!!`)
+}
+else{
+    console.log("Parabéns, você está velocidade correta!")
+}
+console.log(`Dirija sempre com segurança`)
+
+
