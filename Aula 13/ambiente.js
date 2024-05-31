2b4fbe1ac9755c21fb797df5df7905713435e29b
Kaue Negrini Ciglioni <kaueciglioni@hotmail.com>
2024-05-31 15:53:57 -0300

    Continuação do desenvolvimento javascript e repetições e condições simples e compostas

---
+++ Aula 13/ambiente.js
@@ -0,0 +1,11 @@
+var c = 1
+
+do {
+    console.log(`O numero atual é ${c}!`)
+    c++
+    
+
+} while (c <= 6)
+
+
+//console.log('Tudo bem?')
---
+++ Aula 14/ambiente.js
@@ -0,0 +1,5 @@
+console.log('INICIO')
+for (var A = 1; A < 8; A++){
+    console.log(`O numero atual é ${A}`)
+}
+console.log('FIM')
